a230a3f9002c9b8cb9a09aad197243d72049c524
M1NO001 <dntjd1306@naver.com>
2023-09-22 21:57:38 +0900

    5강 41분치

---
+++ Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallLookevent(Vector2 direction) 
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+}
+
+////[SerializeField]private float speed = 5f;
+
+
+//void Update()
+//{
+//    //float x = Input.GetAxisRaw("Horizontal");
+//    //float y = Input.GetAxisRaw("Vertical");
+//    //transform.position += new Vector3(x, y)*Time.deltaTime*speed;
+//}
